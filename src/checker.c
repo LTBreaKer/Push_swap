9f6c47b69e63fe397d0fce7fb8ad0d9d349fa67b
aharrass <abdou.harrassi.7@gmail.com>
2022-12-18 21:40:06 +0100

    before last push

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: aharrass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 21:01:00 by aharrass          #+#    #+#             */
-/*   Updated: 2022/12/16 15:47:20 by aharrass         ###   ########.fr       */
+/*   Updated: 2022/12/18 20:54:19 by aharrass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	main(int ac, char **av)
 			free(tmp);
 		}
 		if (is_sorted(stack_a) && !stack_b)
-			printf("OK\n");
+			ft_putendl_fd("OK", 1);
 		else
-			printf("KO\n");
+			ft_putendl_fd("KO", 1);
 	}
 	clean_lst(&stack_a);
 	clean_lst(&stack_b);
--- src/operations3.c
+++ src/operations3.c
@@ -6,7 +6,7 @@
 /*   By: aharrass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 13:26:35 by aharrass          #+#    #+#             */
-/*   Updated: 2022/12/03 14:12:30 by aharrass         ###   ########.fr       */
+/*   Updated: 2022/12/18 21:36:22 by aharrass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,16 @@ void	ft_rrr(t_stack **stack_a, t_stack **stack_b)
 {
 	ft_rra(stack_a);
 	ft_rrb(stack_b);
-	ft_putstr_fd("rrr\n", 1);
 }
 
 void	ft_rr(t_stack **stack_a, t_stack **stack_b)
 {
 	ft_ra(stack_a);
 	ft_rb(stack_b);
-	ft_putstr_fd("rr\n", 1);
 }
 
 void	ft_ss(t_stack **stack_a, t_stack **stack_b)
 {
 	ft_sa(stack_a);
 	ft_sb(stack_b);
-	ft_putstr_fd("ss\n", 1);
 }
